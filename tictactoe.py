755e05c7f5fd2884efe3fa0af986dcc943f541e0
Alex Rudnick <alex.rudnick@gmail.com>
2022-04-26 15:50:51 -0700

    now with minimax AI that doesn't want to lose

--- tictactoe.py
+++ tictactoe.py
@@ -40,7 +40,6 @@ class BoardState:
             entries = [self.board[i] for i in index_list]
             set_entries = set(entries)
             if len(set_entries) == 1:
-                # print("same:", index_list)
                 if entries[0] in "XO":
                     return entries[0]
         # nobody won yet.
@@ -48,7 +47,7 @@ class BoardState:
 
     def available_moves(self):
         # all the indices that have not yet been taken
-        return [i for i in range(9)
+        return [i+1 for i in range(9)
                 if self.board[i] == " "]
         
     def make_move(self, player, position):
@@ -56,19 +55,18 @@ class BoardState:
 
         Assumes the outside game logic is keeping track of whose turn it is.
         """
-        # 1-based indices for the input
-        index = position - 1
         if player not in "XO":
             print("error: invalid player?")
             return self
 
-        if index not in self.available_moves():
-            print("can't make that move")
+        if position not in self.available_moves():
+            print("can't make that move", position)
+            self.printme()
             return self
         # now we know that's a valid move
         newboard = BoardState()
         newboard.board = self.board[:]
-        newboard.board[index] = player
+        newboard.board[position - 1] = player
         return newboard
 
     def printme(self):
@@ -81,13 +79,83 @@ class BoardState:
 """.format(*self.board))
 
 
+
+def minmax_search(board):
+    # return the move that optimizes the utility for player O
+    possible_moves = board.available_moves()
+    print("considering making one of these:", possible_moves)
+    # for each move that's possible here
+    # pick the move that maximizes my utility if the subsequent move maximizes
+    # the utility for the opponent
+    bestscore = float('-inf')
+    bestmove = None
+    for move in possible_moves:
+        newboard = board.make_move("O", move)
+
+        ## The problem was that we were calling max_value here, rather than
+        ## min_value!
+        score = min_value(newboard)
+        if score > bestscore:
+            bestmove = move
+            bestscore = score
+    return bestmove
+
+# player O is the AI and they're trying to maximize
+# player X is the human and they're trying to minimize
+
+def max_value(board):
+    """find the maximal possible score, where O makes the next move"""
+    if board.winner() == "O":
+        return 1
+    elif board.winner() == "X":
+        return -1
+    elif not board.available_moves():
+        return 0
+    bestscore = float('-inf')
+    possible_moves = board.available_moves()
+    for move in possible_moves:
+        newboard = board.make_move("O", move)
+        board_score = min_value(newboard)
+        if board_score > bestscore:
+            bestscore = board_score
+    return bestscore
+
+def min_value(board):
+    """find the minimal possible score, where X makes the next move"""
+    if board.winner() == "O":
+        return 1
+    elif board.winner() == "X":
+        return -1
+    elif not board.available_moves():
+        return 0
+    bestscore = float('inf')
+    possible_moves = board.available_moves()
+    for move in possible_moves:
+        newboard = board.make_move("X", move)
+        board_score = max_value(newboard)
+        if board_score < bestscore:
+            bestscore = board_score
+    return bestscore
+            
 def main():
     board = BoardState()
 
     cur_player = "X"
     while (not board.winner()) and (board.available_moves()):
         board.printme()
-        position = input("player {} make a move:".format(cur_player))
+
+        # if cur_player is the person, then ask for input
+        if cur_player == "X":
+            position = input("player {} make a move: ".format(cur_player))
+        # if cur_player is the AI, then calculate optimal move
+        elif cur_player == "O":
+            # calculate optimal move
+            bestmove = minmax_search(board)
+            print("got the best move for the board:", bestmove)
+            position = str(bestmove)
+        else:
+            print("oh no")
+            return
 
         try:
             position = int(position)
